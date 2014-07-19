9c37ec6c57262c3263c7f9cba69dcf063cd30a17
karllrak <pcpcppp@163.com>
2014-07-19 21:43:47 +0800

    add current day events highlighting

--- EventItemController.py
+++ EventItemController.py
@@ -2,9 +2,29 @@
 #coding=utf8
 
 import DB
+import datetime
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from ViewEventItem import EventShowWidget
+
+def insertDataToRow( widget, data, rowNumber ):
+    curDate = datetime.datetime.now()
+    startTime = datetime.datetime.strptime( data[1], '%Y-%m-%d %H:%M:%S' )
+    delta = startTime - curDate
+    isToday = True if -1 == delta.days else False
+
+    for j in range( len(data) ):
+	d = data[j]
+	if 0 == d:
+	    d = u''
+	elif 1 == d:
+	    d = u'是'
+	widget.setItem( rowNumber, j, QTableWidgetItem( d ) )
+
+    #highlight today 
+    if isToday:
+	for i in range( len(data) ):
+	    widget.item( rowNumber, i ).setBackground( QBrush( QColor( 180, 128, 180 ) ) )
 
 if '__main__' == __name__:
     import sys
@@ -18,16 +38,9 @@ if '__main__' == __name__:
     w.ui.tableShowWidget.setColumnWidth( 0, int( 1.5 * t.columnWidth( 0 ) ) )
     for i in range( len(data) ):
 	w.ui.tableShowWidget.insertRow( i )
-    #load the data to table
-	for j in range( 5 ):
-	    d = data[i][j]
-	    if 0 == d:
-		d = u''
-	    elif 1 == d:
-		d = u'是'
-	    w.ui.tableShowWidget.setItem( i, j, 
-		    QTableWidgetItem( d ) )
-	
+	#load the data to table
+	insertDataToRow( widget=w.ui.tableShowWidget, data=data[i], rowNumber=i )
+    
     w.ui.tableShowWidget.setHorizontalHeaderLabels(
 	    [u'描述',
 		'startTime',
